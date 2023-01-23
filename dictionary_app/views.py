2ab32a30b0d95984a4b65441324fa1096e040d17
James Johnson <jamesjhnsn1995@gmail.com>
2023-01-23 11:35:40 -0800

    finished styling results page

--- dictionary_app/views.py
+++ dictionary_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from pydictionary import Dictionary
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def word(request):
+    search = request.GET.get('search')
+    dictionary = Dictionary(search)
+    meaning = dictionary.meanings()[0]
+    synonyms = dictionary.synonyms()
+    antonyms = dictionary.antonyms()
+    context = {
+        'meaning': meaning,
+        'synonyms': synonyms,
+        'antonyms': antonyms
+    }
+    
+    return render(request, 'word.html', context=context)
